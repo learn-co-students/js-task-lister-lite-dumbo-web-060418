45692129d873ea0bde73047baf6073f505c7e4fb
QMaximillian <quinnlashinsky@gmail.com>
2018-07-23 17:32:37 -0400

    Done.

--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,16 @@
+//Controller 
+
+
+
 class TaskList {
-}
+	constructor() {
+		this.createTaskForm = document.querySelector('#create-task-form')
+		this.tasks = document.querySelector("#tasks")
+		this.taskDescription = document.querySelector("#new-task-description")
+	}
+	
+	
+	
+
+
+	}
