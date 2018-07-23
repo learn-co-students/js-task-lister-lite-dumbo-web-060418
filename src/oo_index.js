7b0c6b3713161cb75c10283b219a6bf160e8ea44
rscheiwe <rscheiwe@gmail.com>
2018-07-23 15:08:59 -0400

    partially done

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  let tasklist = new TaskList();
+
+  tasklist.initRender();
+  tasklist.newSubmit();
+
 });
+
+
+// function initRender() {
+//   //get and render database users
+//   fetch(`http://localhost:3000/tasks`, {
+//     method: 'GET'
+//   })
+//     .then(res => res.json())
+//     .then(data => {
+//       const taskListed = document.querySelector('#tasks')
+//
+//       const template = data.map(function(task) {
+//         return `<li>${task.description}</li>`
+//       }).join('')
+//       taskListed.innerHTML += template
+//     })
+//   }
--- src/task.js
+++ src/task.js
@@ -1,2 +1,18 @@
 class Task {
+
+  constructor(description) {
+    this.description = description
+
+  
+  fetch('http://localhost:3000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      description: this.description
+    })
+  })
+
+  }
 }
